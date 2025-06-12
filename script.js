081ba0329726e7157cfb0d2d4bf4fcf668964642
NamDK <namdk131@gmail.com>
2025-06-13 00:34:32 +1000

    highlight on click, update footer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-links a");
 
+// Highlight based on scroll
 window.addEventListener("scroll", () => {
     let current = "";
 
@@ -17,5 +18,15 @@ window.addEventListener("scroll", () => {
         if (link.getAttribute("href") === "#" + current) {
             link.classList.add("active");
         }
+    });
+});
+
+// Highlight when clicked
+navLinks.forEach(link => {
+        console.log("hello");
+    link.addEventListener("click", () => {
+        navLinks.forEach(link => link.classList.remove("active"));
+        link.classList.add("active");
+        console.log("hello");
     });
 });
